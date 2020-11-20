aef52b6ee74196249f3f1ec34b3575c3aa72c0e2
MARSHM3LLO2020 <marshmellobravo06@gmail.com>
2020-11-20 13:40:06 +0530

    Debug Angry bird game

--- sketch.js
+++ sketch.js
@@ -81,9 +81,9 @@ function draw(){
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
+    if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+    }
 }
 
 
@@ -94,6 +94,8 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
+        bird.trajectory = []
+        Matter.Body.setPosition(bird.body, {x:200, y: 50})
        slingshot.attach(bird.body);
     }
 }
@@ -105,7 +107,8 @@ async function getBackgroundImg(){
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
     
-    if(hour>=0600 && hour<=1900){
+    console.log(hour);
+    if(hour>=06 && hour<=19){
         bg = "sprites/bg1.png";
     }
     else{
